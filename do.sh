eafe69bb67d7299dca57dd3ea7ed823bacde9e48
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-05-20 15:59:03 +0200

    added LICENCE

--- do.sh
+++ do.sh
@@ -16,6 +16,7 @@ cp ../ecere/dggal/dgbuild/dggal/bindings/rust/ecrt_cffi.rs ./ecrt_sys/src/lib.rs
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/obj/linux/libecrt_sys.rlib ./ecrt_sys/.
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/obj/linux/libecrt_cStatic.a ./ecrt_sys/.
 cp ../ecere/dggal/dgbuild/eC/obj/linux/lib/libecrtStatic.a ./ecrt_sys/.
+cp ../ecere/dggal/LICENSE ./ecrt_sys/.
 
 sed -i 's|/\*unsafe\*/ *extern|unsafe extern|' ./ecrt_sys/src/lib.rs
 sed -i '1i\#![allow(non_upper_case_globals)]' ./ecrt_sys/src/lib.rs
@@ -27,10 +28,12 @@ cp ../ecere/dggal/dgbuild/dggal/bindings/rust/dggal_cffi.rs ./dggal_sys/src/lib.
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/obj/linux/libdggal_sys.rlib ./dggal_sys/.
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/obj/linux/libdggal_cStatic.a ./dggal_sys/.
 cp ../ecere/dggal/dgbuild/dggal/obj/static.linux/libdggalStatic.a ./dggal_sys/.
+cp ../ecere/dggal/LICENSE ./dggal_sys/.
 
 sed -i 's|/\*unsafe\*/ *extern|unsafe extern|' ./dggal_sys/src/lib.rs
 
 
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/dggal.rs ./dggal/src/lib.rs
 cp ../ecere/dggal/dgbuild/dggal/bindings/rust/obj/linux/libdggal.rlib ./dggal/.
+cp ../ecere/dggal/LICENSE ./dggal/.
 
